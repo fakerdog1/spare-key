9fbd8b021efe5012764db768936575178564a91e
fakerdog1 <simeonoff.martin@gmail.com>
2024-08-30 14:55:08 +0300

    remove alpine and tailwind and install jquery and bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import './bootstrap';
+import jQuery from 'jquery';
+import 'bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+window.$ = jQuery;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { glob } from 'glob';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                // Dynamically get all CSS files
+                ...glob.sync('resources/{css,sass,scss}/**/*.{css,sass,scss}'),
+                // Dynamically get all JS files
+                ...glob.sync('resources/js/**/*.js'),
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '$': 'jQuery',
+            '@': '/resources/js',
+        },
+    },
 });
